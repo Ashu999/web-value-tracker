15939997773da098f0b7d6b7547dc3dd9e15cd7b
Ashu999 <ashutosh.sharma068@gmail.com>
2024-08-25 22:42:09 +0530

    init

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './web_value_tracker.js',
+  './web_value_tracker_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
